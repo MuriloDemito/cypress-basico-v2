a5a32f660969fd5431f4b9a836fc7347b305ee52
MuriloDemito <mupoloni321@hotmail.com>
2023-02-05 18:16:05 -0300

    teste curso cypress

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -10,8 +10,117 @@ beforeEach(function(){
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
-    it.only('preenche os campos obrigatórios e envia o formulário', function(){
+    it('preenche os campos obrigatórios e envia o formulário', function(){
+        cy.get('#firstName').type('Murilo Osnei')
+        cy.get('#lastName').type('Poloni Demito')
+        cy.get('#email').type('muriosnei@hotmail.com')
+        cy.get('#open-text-area').type('Automatizando o teste de front-end com Cypress.')
+        cy.contains('button','Enviar').click()
+        cy.get('.success').should('be.visible')
+    })
 
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário',function(){
+        cy.get('#firstName').type('Murilo Osnei')
+        cy.get('#lastName').type('Poloni Demito')
+        cy.get('#email').type('teste@emailinvalido,com')
+        cy.get('#open-text-area').type('Automatizando o teste de front-end com Cypress.')
+        cy.contains('button','Enviar').click()
+        cy.get('.error').should('be.visible')
+    })
+
+    it('campo telefone vazio quando o valor não é numerico',function(){
+        cy.get('#phone')
+            .type('abcdefg')
+            .should('have.value','')
+    })
+
+    it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário',function(){
+        cy.get('#firstName').type('Murilo Osnei')
+        cy.get('#lastName').type('Poloni Demito')
+        cy.get('#email').type('teste@emailinvalido.com')
+        cy.get('#phone-checkbox').check()
+        cy.get('#open-text-area').type('Automatizando o teste de front-end com Cypress.')
+        cy.contains('button','Enviar').click()
+        cy.get('.error').should('be.visible')
+    })
+
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', function(){
+        cy.get('#firstName')
+            .type('Murilo Osnei')
+            .should('have.value','Murilo Osnei')
+            .clear()
+            .should('have.value','')
+        cy.get('#lastName')
+            .type('Poloni Demito')
+            .should('have.value','Poloni Demito')
+            .clear()
+            .should('have.value','')
+        cy.get('#phone')
+            .type('1234567890')
+            .should('have.value','1234567890')
+            .clear()
+            .should('have.value','')        
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios.',function(){
+        cy.contains('button','Enviar').click()
+        cy.get('.error').should('be.visible')
+    })
+
+    it('envia o formuário com sucesso usando um comando customizado.',function(){
+        cy.fillMandatoryFieldsAndSubmit()
+        cy.get('.success').should('be.visible')
+    })
+
+    it('seleciona um produto (YouTube) por seu texto.',function(){
+        cy.get('#product')
+            .select('YouTube')
+            .should('have.value', 'youtube')
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)',function(){
+        cy.get('#product')
+            .select('mentoria')
+            .should('have.value', 'mentoria')
+    })
+
+    it('seleciona um produto (Blog) por seu índice.',function(){
+        cy.get('#product')
+            .select(1)
+            .should('have.value', 'blog')
+    })
+
+    it('marca o tipo de atendimento "Feedback"',function(){
+        cy.get('input[type="radio"][value="feedback"]')
+            .check()
+            .should('have.value', 'feedback')
+    })
+    
+    it('marca cada tipo de atendimento',function(){
+        cy.get('input[type="radio"]')
+            .should('have.length', '3')
+            .each(function($radio) {
+                cy.wrap($radio).check()
+                cy.wrap($radio).should('be.checked')
+            })
+    })
+
+    it('marca ambos checkboxes, depois desmarca o último.',function(){
+        cy.get('input[type="checkbox"]')
+            .check()
+            .should('be.checked')
+            .last()
+            .uncheck()
+            .should('not.be.checked')
+    })
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário.',function(){
+        cy.get('input[type="checkbox"]')
+            .check()
+            .should('be.checked')
+            .last()
+            .uncheck()
+            .should('not.be.checked')
     })
   })
   
