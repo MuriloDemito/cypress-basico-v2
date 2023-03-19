21a4dac9100ba9973221bb32d1f5bce1fbd83fe0
MuriloDemito <mupoloni321@hotmail.com>
2023-03-18 23:11:47 -0300

    ajuste 3

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
-module.exports = {
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {
     setupNodeEvents(on, config) {},
   },
-}
+})
